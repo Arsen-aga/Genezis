8e4961d6091cb327f234f75d9c843263678cc65d
Asanov Arsen <tatarin08082002@gmail.com>
2023-12-30 03:34:08 +0300

    added header

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+ module.exports = {
+   content: ["./app/*.html", "./app/**/*.svg"],
+   theme: {
+     screens: {
+       xs: "375px",
+       sm: "468px",
+       md: "540px",
+       lg: "720px",
+       xl: "960px",
+       "2xl": "1140px",
+       "3xl": "1200px",
+       "4xl": "1360px",
+       "5xl": "1550px",
+     },
+     container: {
+       center: true,
+       padding: {
+         DEFAULT: "20px",
+         xs: "16px",
+         sm: "16px",
+       },
+     },
+     fontFamily: {
+       "manrope-700": "Manrope-Bold",
+       "manrope-600": "Manrope-SemiBold",
+       "manrope-500": "Manrope-Medium",
+       "manrope-400": "Manrope-Regular",
+     },
+     extend: {
+       colors: {
+         accent: "#00587A",
+         default: "#202020",
+         brand: "#EF7F1A",
+         milk: "#FFFAFA",
+         gray: "#686868",
+         divider: "#E5E2EE",
+         neutrallight: "#C4C5D9",
+       },
+       backgroundImage: {
+         "section-1": "url('/app/img/bg.jpg')",
+       },
+     },
+   },
+   plugins: [],
+ };
